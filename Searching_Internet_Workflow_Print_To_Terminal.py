681f080ad847bf76f8969152006e724577050bf3
Zabby Heying <heyingz@fultonschools.org>
2025-12-05 15:40:21 +0000

    Added example of basic internet search without file storage

---
+++ Searching_Internet_Workflow_Print_To_Terminal.py
@@ -0,0 +1,26 @@
+from langchain_ollama import ChatOllama
+from langchain_core.prompts import ChatPromptTemplate  
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.tools import DuckDuckGoSearchRun
+
+
+model = ChatOllama(model="research-llm")
+prompt = ChatPromptTemplate.from_template("""You are a helpful research AI. Use the following web search result: {context}. To write a concise, well-structured response to the question /"Who is : {question} /" """)
+parser = StrOutputParser()
+search = DuckDuckGoSearchRun()
+
+chain = prompt | model | parser
+
+def generate_response(userText):
+    context = search.invoke(f"Who is {userText}")
+    response = chain.invoke({"question": f"Summarize information about {userText}", "context": context})
+    return response
+
+count = 1
+userInput = input("Write the name of the person you would like to learn about today below: \nType exit to leave\n\n")
+while userInput.lower() != 'exit':
+    aiResponse = generate_response(userInput)
+    print(userInput, aiResponse, count)
+    userInput = input("Who else do you want to learn about? *You can always type exit to leave*\n")
+    count += 1
+print("Good-bye. Thank you for using my program")
